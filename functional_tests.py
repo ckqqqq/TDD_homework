7be397829024968d829aab3be510e538cd5eb878
hlx <1227187196@qq.com>
2022-06-14 14:48:55 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -3,11 +3,15 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
+
     def tearDown(self):
         self.browser.quit()
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -24,6 +28,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feather' for row in rows)
+            any(row.text == '1: Buy peacock feather' for row in rows),
+            "New to-do item did not appear in table"
         )
         self.fail('Finish the test!')
